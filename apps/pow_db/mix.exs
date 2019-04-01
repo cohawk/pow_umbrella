edfa098089f4673e2a0d2372a7658076e1f6c721
cohawk <bpcohee@gmail.com>
2019-03-31 20:51:33 -0400

    There are cycles in the dependency graph

--- apps/pow_db/mix.exs
+++ apps/pow_db/mix.exs
@@ -31,7 +31,9 @@ defmodule PowDb.MixProject do
       # {:sibling_app_in_umbrella, in_umbrella: true}
       {:pow, "~> 1.0.4"},
       {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"}
+      {:postgrex, ">= 0.0.0"},
+
+      {:pow_phx, in_umbrella: true}
     ]
   end
 end
